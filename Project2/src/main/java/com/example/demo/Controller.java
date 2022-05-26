49eeeaf2cbe9093e67e71c3f8d07d3b94fd80a0f
Roshini503 <106264165+Roshini503@users.noreply.github.com>
2022-05-26 14:39:56 -0500

    test commit

---
+++ Project2/src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class Controller {
+	
+	@GetMapping("/getMessage")
+	
+	public String getMessage(Model model) {
+		model.addAttribute("Message", "Hello World");
+		return "Hello!";
+	}
+}
